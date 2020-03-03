975d52300b21f3e619665212c4a162f36813da4a
InturiTejaswini <chowdarytejaswini2@gmail.com>
2020-03-03 18:38:30 +0530

    [Tejaswini]Refactor.changes in note component bl

--- src/components/remainder.js
+++ src/components/remainder.js
@@ -1,11 +1,10 @@
 
 
 import React, { useState } from "react";
-import { Button, View } from "react-native";
+import { View } from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { Image, TouchableOpacity } from 'react-native'
 import styles from '../Css';
-
 const Example = () => {
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
