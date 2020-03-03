9348c10f9272a5ecaa2cc7a1590405e785e51672
InturiTejaswini <chowdarytejaswini2@gmail.com>
2020-03-03 17:07:58 +0530

    [Tejaswini]Refactor.changes in note component bl

--- src/components/notes.js
+++ src/components/notes.js
@@ -11,7 +11,7 @@ export class Notes extends React.Component {
             visible: false,
         }
     }
-   
+
     render() {
         return (
             <View>
@@ -24,8 +24,8 @@ export class Notes extends React.Component {
                     <Image source={require("../assets/pushpin.jpeg")} style={styles.pushpinicon}></Image>
                 </View>
                 <View >
-                    <Image source={require("../assets/alert.png")} 
-                    style={styles.alerticon} ></Image>
+                    <Image source={require("../assets/alert.png")}
+                        style={styles.alerticon} ></Image>
                 </View>
                 <View>
                     <Image source={require("../assets/archive.png")} style={styles.archiveicon}></Image>
