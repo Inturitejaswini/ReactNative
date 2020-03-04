a89c75f1e7e32ab96bf80c67d7c1366817cc38c1
InturiTejaswini <chowdarytejaswini2@gmail.com>
2020-03-04 10:43:59 +0530

    [Tejaswini]Refactor.changes in note component bl

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -6,16 +6,16 @@ import Config from 'react-native-config'
 
 export default function register(data){
     console.warn("register data",data,"url",Config.BASE_URL+configApi.registration)
-    return axios.post(Config.BASE_URL+configApi.registration,data)
+    return axios.post(Config.REACT_APP_BASE_URL+configApi.registration,data)
     }
     
 export function Login(data){
     console.warn("login data",data,"url",Config.BASE_URL+configApi.login)
-    return axios.post(Config.BASE_URL+configApi.login,data)
+    return axios.post(Config.REACT_APP_BASE_URL+configApi.login,data)
 
 }
 export function Fotget(data){
     console.warn("forgetpassword data",data,Config.BASE_URL+configApi.forgetPassword)
-    return axios.post(Config.BASE_URL+configApi.forgetPassword,data)
+    return axios.post(Config.REACT_APP_BASE_URL+configApi.forgetPassword,data)
 
 }
