192cf4ed1bed498d97ce9d2afa5cf5cb1c825180
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-06-16 09:51:50 +0530

    [Tejaswini].refactor some changes ediarchivecomponent

--- src/components/editArchive.js
+++ src/components/editArchive.js
@@ -1,34 +1,15 @@
 import React, { Component } from "react";
-import Icon from "react-native-vector-icons/MaterialIcons";
+import {Icon,Icon3} from "react-native-vector-icons/MaterialIcons";
 import Icon1 from "react-native-vector-icons/MaterialCommunityIcons";
-import Icon2 from "react-native-vector-icons/AntDesign";
-import Icon3 from "react-native-vector-icons/MaterialIcons";
+import {Icon2,Icon0,Icon5} from "react-native-vector-icons/AntDesign";
 import Icon4 from "react-native-vector-icons/FontAwesome";
-import Icon0 from "react-native-vector-icons/AntDesign";
-import Icon5 from "react-native-vector-icons/AntDesign";
-import RBSheet from "react-native-raw-bottom-sheet";
-import RBSheet1 from "react-native-raw-bottom-sheet";
-import RBSheet2 from "react-native-raw-bottom-sheet";
-import { IconButton, Colors } from "react-native-paper";
+import {RBSheet,RBSheet2} from "react-native-raw-bottom-sheet";
+import { IconButton } from "react-native-paper";
 import styles from "../Styles";
-import { editNotes } from '../services/noteServices'
-import { UnArchiveNotes } from '../services/noteServices'
-import { deleteNotes } from '../services/noteServices'
-import { pinNotes } from '../services/noteServices'
-import {UnpinNotes} from '../services/noteServices'
-import { updateColor } from '../services/noteServices'
-import { createLabels } from '../services/noteServices'
+import { editNotes,UnArchiveNotes,deleteNotes,pinNotes,UnpinNotes,updateColor,createLabels} from '../services/noteServices'
 import ReminderComponent from '../components/remainder'
-import {
-    ScrollView,
-    Text,
-    TextInput,
-    View,
-    FlatList,
-    TouchableOpacity
-} from "react-native";
+import {Text, TextInput, View,FlatList,TouchableOpacity} from "react-native";
 const colors = [
-    // { name: "blue", hexcode: "#F5F5DC" }
     { name: "blue", hexcode: " #39a78e" },
     { name: "violet", hexcode: "#7DCEA0" },
     { name: "blue", hexcode: "#76D7C4" },
@@ -67,87 +48,70 @@ export class EditArchiveComponent extends Component {
         this.reminderData = this.reminderData.bind(this);
     }
     reminderData = (value) => {
-        console.warn("dataaa---->", value);
         this.setState({
             reminderDate: value
         });
-        console.warn("date and time ", this.state.reminderDate);
     };
-    handlePin = async () => {
-        await this.setState({
+    handlePin =  () => {
+         this.setState({
             isPined: true
         });
         let data = {
             noteIdList: [this.props.navigation.state.params.key],
             isPined: this.state.isPined,
         }
-        console.warn("log of pined", data);
         pinNotes(data).then(res => {
-            console.warn("response in pin notes", res);
         });
     };
-    handleUnPin = async () => {
-        await this.setState({
+    handleUnPin =  () => {
+         this.setState({
             isPined: false
         });
         let data = {
             noteIdList: [this.props.navigation.state.params.key],
             isPined: this.state.isPined,
         }
-        console.warn("log of pined", data);
         UnpinNotes(data).then(res => {
-            console.warn("response in unpin notes", res);
         });
     };
-    handleDelete = async () => {
-        await this.setState({ isDeleted: true })
+    handleDelete =  () => {
+         this.setState({ isDeleted: true })
         let data = {
             noteIdList: [this.props.navigation.state.params.key],
             isDeleted: this.state.isDeleted
         };
-        console.warn("delete after set state", data);
         deleteNotes(data).then(res => {
-            console.warn("response in delete notes", res);
         });
     };
-    handleUnArchiveNote = async () => {
-        await this.setState({ isArchived: false })
-        console.log("archive data", this.state.isArchived);
+    handleUnArchiveNote = () => {
+         this.setState({ isArchived: false })
         let data = {
             noteIdList: [this.props.navigation.state.params.key],
             isArchived: this.state.isArchived
         };
         UnArchiveNotes(data).then(res => {
-            console.log("res in unarchive component", res);
         });
-        // this.props.navigation.navigate("dashboard")
     };
-    handleLabelDone = async () => {
-        await this.setState({ label: true })
-        console.warn("label Data", this.state.label);
+    handleLabelDone =  () => {
+        this.setState({ label: true })
         let data = {
             noteIdList: [this.props.navigation.state.params.key],
             label: this.state.label
         };
-        console.warn("label data", data);
         createLabels(data).then(res => {
-            console.log(" response from label data", res);
         });
     };
-    handleSelectLabel = async (labelName) => {
-        console.log("id of label", labelName);
-        await this.setState({
+    handleSelectLabel = (labelName) => {
+        this.setState({
             selectedLabels: labelName
         });
-        console.warn("label  setstate", this.state.selectedLabels);
     };
-    handleLabelArrow = async (labelValue) => {
+    handleLabelArrow = (labelValue) => {
         this.RBSheet2.close();
         this.RBSheet.close();
-       await this.setState({
+        this.setState({
             labelValue: labelValue
         });
-        console.warn("labelValue", this.state.labelValue);
     };
     handleEditCard = () => {
         let data = {
@@ -161,26 +125,22 @@ export class EditArchiveComponent extends Component {
             color: this.state.color,
             labelValue: this.state.labelValue
         };
-        console.warn("editnote data", data);
         editNotes(data).then(res => {
-            console.warn("response from edit note data", res);
             this.setState({
                 color: res.color
             })
         });
         this.props.navigation.navigate("archiveComponent");
     };
-    handleColor = async (color) => {
-        await this.setState({
+    handleColor =(color) => {
+        this.setState({
             color: color
         });
         let data = {
             noteIdList: [this.props.navigation.state.params.key],
             color: this.state.color,
         };
-        console.warn("data of updateColor", data);
         updateColor(data).then(res => {
-            console.warn(" response from  color updation", res)
         });
     };
     componentDidMount() {
@@ -194,32 +154,13 @@ export class EditArchiveComponent extends Component {
             labelValue: this.props.navigation.state.params.display.labelValue
         })
     }
-    // componentWillMount() {
-    //     getLabels().then(response => {
-    //       console.log("res in label notes", response);
-    //       this.setState({
-    //         labelData: response
-    //       });
-    //       console.log("label data after setting state", this.state.labelData);
-    //     });
-    //   }
     render() {
-        // let labelDetails = this.state.labelData.map(key1 => {
-        //     console.log("key in label component---->", key1.data().label);
-        //     return (
-        //         <View>
-        //             <CheckBox
-        //                 title={key1.data().label}
-        //                 onPress={() => this.handleSelectLabel(key1.data().label)} />
-        //         </View>
-        //     );
-        // });
         return (
             <View style={{
                 backgroundColor: this.state.color,
                 height: "100%"}}>
-                <View style={{ flexDirection: "row", margin: 10 }}>
-                    <View style={{ width: "60%" }}>
+                <View style={styles.view}>
+                    <View>
                         <Icon1
                             name="arrow-left"
                             size={25}
@@ -245,7 +186,7 @@ export class EditArchiveComponent extends Component {
                         <View>
                             <ReminderComponent reminderProps={this.reminderData} ></ReminderComponent>
                         </View>
-                        <View style={{ left: 20 }}>
+                        <View style={styles.unarchive}>
                             <Icon3
                                 name="unarchive"
                                 size={25}
@@ -256,28 +197,25 @@ export class EditArchiveComponent extends Component {
                 <View style={styles.noteData}>
                     <View>
                         <TextInput
-                            style={{ height: 60, fontSize: 22, left: 20 }}
                             placeholder="Tittle"
                             defaultValue={this.props.navigation.state.params.display.title}
                             onChangeText={title => this.setState({ title })}
                             value={this.state.title}
                         />
                     </View>
-                    <View style={{ marginTop: -10, left: 23 }}>
+                    <View style={styles.textInput}>
                         <TextInput
-                            style={{ height: 60, fontSize: 17 }}
                             placeholder="note"
                             defaultValue={this.props.navigation.state.params.display.description}
                             value={this.state.description}
                             onChangeText={description => this.setState({ description })}
                         />
                     </View>
-                    <Text style={{ fontWeight: "bold", left: 10 }}>
+                    <Text style={styles.reminder}>
                         {this.state.reminder}
                     </Text>
                 </View>
-                <View
-                    style={{ top: 400, left: 340 }}>
+                <View>
                     <Icon4
                         name="ellipsis-v"
                         size={25}
@@ -289,51 +227,30 @@ export class EditArchiveComponent extends Component {
                         customStyles={{
                             container: {
                                 flexDirection: "column",
-                                // bottom:45
                             }
                         }}>
-                        <View style={{
-                            flexDirection: "row",
-                            left: 10,
-                            marginTop: 18
-                        }}>
+                        <View style={styles.delete}>
                             <TouchableOpacity onPress={() => this.handleDelete()}>
                                 <Icon0
                                     name="delete"
                                     size={20} />
-                                <Text style={{ fontSize: 18, left: 38, top: -20 }}>Delete</Text>
+                                <Text>Delete</Text>
                             </TouchableOpacity>
                         </View>
-                        <View
-                            style={{
-                                flexDirection: "row",
-                                left: 10,
-                                // marginTop: 18
-                            }}>
+                        <View>
                             <Icon2 name="sharealt" size={22} />
-                            <Text style={{ fontSize: 18, left: 20 }}>send</Text>
+                            <Text style={styles.send}>send</Text>
                         </View>
                         <View
-                            style={{
-                                flexDirection: "row",
-                                left: 10,
-                                marginTop: 18
-                            }}>
+                            style={styles.collaborator}>
                             <Icon2 name="addusergroup" size={25} />
-                            <Text
-                                style={{ fontSize: 18, left: 20 }}
+                            <Text style={styles.collaboratorText}
                                 onPress={() => { this.RBSheet1.open() }}>collaborator</Text>
                         </View>
-                        <View
-                            style={{
-                                flexDirection: "row",
-                                left: 10,
-                                marginTop: 18
-                            }} >
+                        <View>
                             <Icon1 name="label-outline" size={25} />
                             <Text
-                                onPress={() => { this.RBSheet2.open(); }}
-                                style={{ fontSize: 18, left: 20 }}>Labels</Text>
+                                onPress={() => { this.RBSheet2.open(); }}>Labels</Text>
                         </View>
                         <View>
                             <FlatList
@@ -375,27 +292,20 @@ export class EditArchiveComponent extends Component {
                             </View>
                             <View>
                                 <TextInput
-                                    style={{
-                                        height: 40,
-                                        fontSize: 18,
-                                        left: 30,
-                                        marginTop: -3
-                                    }}
+                                    style={styles.label}
                                     placeholder="Enter label name"
                                     value={this.state.label}
                                     onChangeText={label => this.setState({ label })}
                                 />
                             </View>
-                            <View style={{ left: 150 }}>
+                            <View>
                                 <Icon
                                     name="done"
                                     size={25}
                                     onPress={() => {
-                                        this.handleLabelDone();
-                                    }} />
+                                        this.handleLabelDone();}} />
                             </View>
                         </View>
-                        {/* <ScrollView>{labelDetails}</ScrollView> */}
                     </RBSheet2>
                 </View>
             </View>
