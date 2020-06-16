caac7d794bbe7763c276a69a4c160075ff88831f
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-06-16 19:11:29 +0530

    [Tejaswini].refactor  some changes in userservices component

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -2,31 +2,31 @@ import axios from 'axios'
 import configApi from '../constants/userApiConstants'
 import Config from 'react-native-config'
 import AsyncStorage from '@react-native-community/async-storage'
-export default function register(data){
-    return axios.post(Config.REACT_APP_BASE_URL+configApi.registration,data)
-    }
-    
-export function login(data){
-    return axios.post(Config.REACT_APP_BASE_URL+configApi.login,data)
-
-}
-export function forget(data){
-    return axios.post(Config.REACT_APP_BASE_URL+configApi.forgetPassword,data)
-
+export default function register(data) {
+    return axios.post(Config.REACT_APP_BASE_URL + configApi.registration, data)
 }
 
-export function userLogOut(data){
-    return axios.post(Config.REACT_APP_BASE_URL+configApi.signOut,data)
+export function login(data) {
+    return axios.post(Config.REACT_APP_BASE_URL + configApi.login, data)
+
+}
+export function forget(data) {
+    return axios.post(Config.REACT_APP_BASE_URL + configApi.forgetPassword, data)
 
 }
 
+export function userLogOut(data) {
+    return axios.post(Config.REACT_APP_BASE_URL + configApi.signOut, data)
 
-export async function uploadProfile(){
+}
+
+
+export async function uploadProfile() {
     let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL+configApi.profileUpdate,
+    let res = axios.post(Config.REACT_APP_BASE_URL + configApi.profileUpdate,
         {
             headers: {
-                Authorization:AccessToken
+                Authorization: AccessToken
             }
         })
     return res
