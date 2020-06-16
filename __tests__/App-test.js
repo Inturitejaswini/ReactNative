92c894181eeb6f50526c86e065b7f83f28922548
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-06-16 12:27:02 +0530

    [Tejaswini].refactor  modified changes

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -6,8 +6,6 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-// Note: test renderer must be required after react-native.
 
 import renderer from 'react-test-renderer';
 
