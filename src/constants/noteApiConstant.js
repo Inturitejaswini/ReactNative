819287dbbbfc3b390ef49ba691edd331fafdb462
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-06-16 19:13:43 +0530

    [Tejaswini].refactor  some changes in userapiconstantcomponent

--- src/constants/noteApiConstant.js
+++ src/constants/noteApiConstant.js
@@ -1,18 +1,18 @@
-var noteConfigApi={
-    noteAdd:"/notes/addNotes",
-    getNotes:"/notes/getNotesList",
-    editNotes:"/notes/updateNotes",
-    archiveNotes:"/notes/archiveNotes",
-    deleteNotes:"/notes/trashNotes",
-    pinNotes:"/notes/pinUnpinNotes",
-    UnPinNotes:"/notes/pinUnpinNotes",
-    colorNotes:"/notes/changesColorNotes",
-    updateReminder:"/notes/addUpdateReminderNotes",
-    addLabel:"/noteLabels",
-    deleteForever:"/notes/deleteForeverNotes",
-    restoreNotes:"/notes/deleteForeverNotes",
-    unarchiveNotes:"/notes/archiveNotes",
-    notecollaborator:"/notes/%7B%225e688eddad53b700227c581c%22%7D/AddcollaboratorsNotes",
-    getLabels:"/noteLabels/getNoteLabelList"
-}             
+var noteConfigApi = {
+    noteAdd: "/notes/addNotes",
+    getNotes: "/notes/getNotesList",
+    editNotes: "/notes/updateNotes",
+    archiveNotes: "/notes/archiveNotes",
+    deleteNotes: "/notes/trashNotes",
+    pinNotes: "/notes/pinUnpinNotes",
+    UnPinNotes: "/notes/pinUnpinNotes",
+    colorNotes: "/notes/changesColorNotes",
+    updateReminder: "/notes/addUpdateReminderNotes",
+    addLabel: "/noteLabels",
+    deleteForever: "/notes/deleteForeverNotes",
+    restoreNotes: "/notes/deleteForeverNotes",
+    unarchiveNotes: "/notes/archiveNotes",
+    notecollaborator: "/notes/%7B%225e688eddad53b700227c581c%22%7D/AddcollaboratorsNotes",
+    getLabels: "/noteLabels/getNoteLabelList"
+}
 export default noteConfigApi
