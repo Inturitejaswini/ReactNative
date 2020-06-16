365395e25b0c2276909972db6861814065022160
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-06-16 15:30:30 +0530

    [Tejaswini].refactor  some changes in search component

--- src/components/searchComponent.js
+++ src/components/searchComponent.js
@@ -16,13 +16,10 @@
 import React, { Component } from 'react'
 import { View, Button, Text, TouchableOpacity, TextInput } from 'react-native'
 import styles from '../Styles';
-import Icon1 from "react-native-vector-icons/MaterialCommunityIcons";
+import { Icon1, Icon2, Icon } from "react-native-vector-icons/MaterialCommunityIcons";
 import { Divider, Card } from 'react-native-paper';
-import Icon2 from "react-native-vector-icons/MaterialCommunityIcons";
 import { getNotes } from "../services/noteServices";
 import Icon4 from "react-native-vector-icons/Ionicons";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-
 export class SearchComponent extends Component {
     constructor() {
         super();
@@ -38,34 +35,30 @@ export class SearchComponent extends Component {
     }
     componentDidMount() {
         this.getNotes();
-        // this.getImage();
     }
     getNotes = () => {
         getNotes().then(res => {
-            console.log("res in get notes", res);
             this.setState({
                 notes: res
             });
         });
     };
-    handleSearchArrow = async () => {
-        await this.setState({
+    handleSearchArrow = () => {
+        this.setState({
             searchOpen: false
         });
         this.props.navigation.navigate('dashboard')
     };
-    handleSearchValue = async value => {
-        console.log("search value", value);
-        await this.setState({
+    handleSearchValue = (value) => {
+        this.setState({
             searchValue: value
         });
-        console.warn("search after set state", this.state.searchValue);
     };
     render() {
 
         return (
             <View >
-                <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                <View style={styles.searchView}>
                     <View >
                         <TouchableOpacity onPress={() => { this.handleSearchArrow() }} >
                             <Icon1
@@ -75,9 +68,10 @@ export class SearchComponent extends Component {
                     </View>
                     <View>
                         <TextInput
-                            style={{ height: 40, fontSize: 20, right: -20 }}
+                            style={styles.searchInput}
                             placeholder="search your notes...."
-                            onChangeText={this.handleSearchValue} /></View>
+                            onChangeText={this.handleSearchValue} />
+                    </View>
                 </View>
                 <Divider type='horizontal' style={{ height: 2 }}></Divider>
                 <View style={styles.typetext}>
@@ -107,13 +101,13 @@ export class SearchComponent extends Component {
                     <Text>Labels</Text>
                 </View>
                 <View>
-                        <TouchableOpacity>
-                            <Card style={styles.labelcard}>
-                                <Icon name="label-outline" size={20} style={styles.labelicon} />
-                                <Text style={styles.labeltext1}>Labels</Text>
-                            </Card>
-                        </TouchableOpacity>
-                    </View>
+                    <TouchableOpacity>
+                        <Card style={styles.labelcard}>
+                            <Icon name="label-outline" size={20} style={styles.labelicon} />
+                            <Text style={styles.labeltext1}>Labels</Text>
+                        </Card>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
