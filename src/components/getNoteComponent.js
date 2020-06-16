5d8da7f3b771c01510f30483ace12c3108a11160
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-06-16 11:28:43 +0530

    [Tejaswini].refactor some changes getNote component

--- src/components/getNoteComponent.js
+++ src/components/getNoteComponent.js
@@ -1,23 +1,17 @@
 import React, { Component } from "react";
-import { Image } from "react-native";
-import { ScrollView, Text, View, TouchableOpacity } from "react-native";
+import { ScrollView, Text, View, TouchableOpacity, Image } from "react-native";
 import { Card } from "react-native-elements";
 import styles from "../Styles";
 import { getNotes } from '../services/noteServices'
-import { Chip } from "react-native-paper";
 export class GetNoteComponent extends Component {
   constructor() {
     super();
     this.state = {
       notes: [],
       open: false,
-      listOpen:false
+      listOpen: false
     };
   }
-  // static navigationOptions = {
-  //   drawerLabel: "Notes",
-  //   drawerIcon: <Icon2 name="bulb1" size={20} />
-  // };
 
   componentDidMount = () => {
     this.getAllNotes();
@@ -28,37 +22,34 @@ export class GetNoteComponent extends Component {
         this.setState({
           notes: res.data.data.data
         })
-        console.log("getNote data ", this.state.notes)
-
       })
     this.setState({
       open: false
     })
   }
-  
+
   render() {
     let Align = this.state.listOpen ? styles.gridAlign : styles.listAlign;
     let noteDetails = this.state.notes.map(key => {
-      // let notes=key.data;
       return (
-         <View style={Align}>
+        <View style={Align}>
           <ScrollView>
-          <TouchableOpacity
-                onPress={() =>
-                  this.props.navigation.navigate("editComponent", {
-                    display: key,
-                    key: key.id
-                  })}>
-            <Card >
-              <View>
-                <Text style={{ fontWeight: "bold" }}>{key.title}</Text>
-                <Text>{key.description}</Text>
-                <Text style={{ fontWeight: "bold" }}>{key.reminder}</Text>
-              </View>
-            </Card>
+            <TouchableOpacity
+              onPress={() =>
+                this.props.navigation.navigate("editComponent", {
+                  display: key,
+                  key: key.id
+                })}>
+              <Card >
+                <View>
+                  <Text style={styles.GetNoteTitle}>{key.title}</Text>
+                  <Text>{key.description}</Text>
+                  <Text style={styles.getNoteReminder}>{key.reminder}</Text>
+                </View>
+              </Card>
             </TouchableOpacity>
           </ScrollView>
-          </View>
+        </View>
       );
     });
     return <View style={styles.getNoteCard}>{noteDetails}</View>
