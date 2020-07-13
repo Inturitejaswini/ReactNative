55e3fa4a813c86a030f0f9549b97e3e61a2663ce
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-13 20:58:53 +0530

    [tejaswini]refactor changes in note services file

--- src/services/noteServices.js
+++ src/services/noteServices.js
@@ -1,167 +1,199 @@
-import axios from 'axios'
-import Config from 'react-native-config'
-import noteConfigApi from '../constants/noteApiConstant'
-import AsyncStorage from '@react-native-community/async-storage'
+import axios from 'axios';
+import Config from 'react-native-config';
+import noteConfigApi from '../constants/noteApiConstant';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export async function createNotes(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.noteAdd, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(
+    Config.REACT_APP_BASE_URL + noteConfigApi.noteAdd,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
 export async function getNotes() {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.get(Config.REACT_APP_BASE_URL + noteConfigApi.getNotes,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.get(Config.REACT_APP_BASE_URL + noteConfigApi.getNotes, {
+    headers: {
+      Authorization: AccessToken,
+    },
+  });
+  return res;
 }
 
 export async function editNotes(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.editNotes, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(
+    Config.REACT_APP_BASE_URL + noteConfigApi.editNotes,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
 
 export async function archiveNotes(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.archiveNotes, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(
+    Config.REACT_APP_BASE_URL + noteConfigApi.archiveNotes,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
 
 export async function UnArchiveNotes(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.unarchiveNotes, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(
+    Config.REACT_APP_BASE_URL + noteConfigApi.unarchiveNotes,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
 export async function deleteNotes(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.deleteNotes, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(
+    Config.REACT_APP_BASE_URL + noteConfigApi.deleteNotes,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
 
 export async function pinNotes(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.pinNotes, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(
+    Config.REACT_APP_BASE_URL + noteConfigApi.pinNotes,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
 
 export async function UnpinNotes(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.UnPinNotes, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(
+    Config.REACT_APP_BASE_URL + noteConfigApi.UnPinNotes,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
 
 export async function updateColor(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.colorNotes, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(
+    Config.REACT_APP_BASE_URL + noteConfigApi.colorNotes,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
 
 export async function createLabels() {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.addLabel,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.addLabel, {
+    headers: {
+      Authorization: AccessToken,
+    },
+  });
+  return res;
 }
 
-
 export async function deleteForever(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.deleteForever, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(
+    Config.REACT_APP_BASE_URL + noteConfigApi.deleteForever,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
 
 export async function restore(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.restoreNotes, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(
+    Config.REACT_APP_BASE_URL + noteConfigApi.restoreNotes,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
 
 export async function noteCollaborator(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.post(Config.REACT_APP_BASE_URL + noteConfigApi.notecollaborator, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.post(
+    Config.REACT_APP_BASE_URL + noteConfigApi.notecollaborator,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
 export async function getAllLabels() {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.get(Config.REACT_APP_BASE_URL + noteConfigApi.getLabels,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.get(Config.REACT_APP_BASE_URL + noteConfigApi.getLabels, {
+    headers: {
+      Authorization: AccessToken,
+    },
+  });
+  return res;
 }
 
 export async function reminderUpdate(data) {
-    let AccessToken = await AsyncStorage.getItem('@storage_Key')
-    let res = axios.get(Config.REACT_APP_BASE_URL + noteConfigApi.updateReminder, data,
-        {
-            headers: {
-                Authorization: AccessToken
-            }
-        })
-    return res
+  let AccessToken = await AsyncStorage.getItem('@storage_Key');
+  let res = axios.get(
+    Config.REACT_APP_BASE_URL + noteConfigApi.updateReminder,
+    data,
+    {
+      headers: {
+        Authorization: AccessToken,
+      },
+    },
+  );
+  return res;
 }
