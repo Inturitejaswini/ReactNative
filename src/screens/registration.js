f8a2df4881787e529dbec88e473fc96121619898
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-13 20:57:06 +0530

    [tejaswini]refactor registration screen code

--- src/screens/registration.js
+++ src/screens/registration.js
@@ -1,25 +1,25 @@
 /******************************************************************************
-* Execution : 1. default node cmd> node registration.js
-* 2. if nodemon installed cmd> nodemodule registration.js
-* 
-* Purpose : create registration page.
-* @description 
-* 
-* @file :registration
-* @overview :registration form problem.
-* @module :registration - This is optional if expeclictly its an npm or local package
-* @author :tejaswini<chowdarytejaswini2@gmail.com>
-* @version :0.61.5
-* @since :-23-02-2020
-******************************************************************************/
+ * Execution : 1. default node cmd> node registration.js
+ * 2. if nodemon installed cmd> nodemodule registration.js
+ *
+ * Purpose : create registration page.
+ * @description
+ *
+ * @file :registration
+ * @overview :registration form problem.
+ * @module :registration - This is optional if expeclictly its an npm or local package
+ * @author :tejaswini<chowdarytejaswini2@gmail.com>
+ * @version :0.61.5
+ * @since :-23-02-2020
+ ******************************************************************************/
 
-import React, { Component } from 'react'
-import { View, Button, Text, Alert } from 'react-native'
+import React, {Component} from 'react';
+import {View, Button, Text, Alert} from 'react-native';
 import styles from '../Styles';
-import register from '../services/userServices'
-import { Card } from 'react-native-elements';
-import Snackbar from "react-native-snackbar-component";
-import { TextInput, ScrollView } from 'react-native-gesture-handler';
+import register from '../services/userServices';
+import {Card} from 'react-native-elements';
+import Snackbar from 'react-native-snackbar-component';
+import {TextInput, ScrollView} from 'react-native-gesture-handler';
 export class RegisterComponent extends Component {
   constructor() {
     super();
@@ -31,46 +31,44 @@ export class RegisterComponent extends Component {
       password: '',
       snackIsVisible: false,
       visible: false,
-
-    }
+    };
   }
-  x
+  x;
   handleFirstName = (event) => {
-    this.setState({ firstName: event });
+    this.setState({firstName: event});
   };
   handleLastName = (event) => {
-    this.setState({ lastName: event });
+    this.setState({lastName: event});
   };
   handlePhoneNumber = (event) => {
-    this.setState({ phoneNumber: event });
+    this.setState({phoneNumber: event});
   };
   handleEmail = (event) => {
-    this.setState({ email: event });
+    this.setState({email: event});
   };
   handlePassword = (event) => {
-    this.setState({ password: event });
+    this.setState({password: event});
   };
   handleRegister = () => {
-    if (this.state.firstName === "") {
+    if (this.state.firstName === '') {
       this.setState({
-        snackIsVisible: !this.state.snackIsVisible
+        snackIsVisible: !this.state.snackIsVisible,
       });
-    } else if (this.state.lastName === "") {
+    } else if (this.state.lastName === '') {
       this.setState({
-        snackIsVisible: !this.state.snackIsVisible
+        snackIsVisible: !this.state.snackIsVisible,
       });
-    }
-    else if (this.state.phoneNumber === "") {
+    } else if (this.state.phoneNumber === '') {
       this.setState({
-        snackIsVisible: !this.state.snackIsVisible
+        snackIsVisible: !this.state.snackIsVisible,
       });
-    } else if (this.state.email === "") {
+    } else if (this.state.email === '') {
       this.setState({
-        snackIsVisible: !this.state.snackIsVisible
+        snackIsVisible: !this.state.snackIsVisible,
       });
-    } else if (this.state.password === "") {
+    } else if (this.state.password === '') {
       this.setState({
-        snackIsVisible: !this.state.snackIsVisible
+        snackIsVisible: !this.state.snackIsVisible,
       });
     } else {
       const user = {
@@ -79,15 +77,13 @@ export class RegisterComponent extends Component {
         phoneNumber: this.state.phoneNumber,
         email: this.state.email,
         password: this.state.password,
-        service: "advance",
-      }
-      register(user).then((res) => {
-        return res
-      })
+        service: 'advance',
+      };
+      register(user);
     }
-  }
+  };
   handleLogin = () => {
-    this.props.history.push('./login')
+    this.props.history.push('./login');
   };
   render() {
     return (
@@ -97,72 +93,67 @@ export class RegisterComponent extends Component {
           visible={this.state.snackIsVisible}
           textMessage="enter the requirements"
           actionHandler={() => {
-            alert("fill all the details of registration");
+            alert('fill all the details of registration');
             this.setState({
-              snackIsVisible: !this.state.snackIsVisible
+              snackIsVisible: !this.state.snackIsVisible,
             });
           }}
           actionText="let's go"
-          distanceCallback={distance => {
-            this.setState({ distance: distance });
-          }} />
+          distanceCallback={(distance) => {
+            this.setState({distance: distance});
+          }}
+        />
         <Card style={styles.cardContainer1}>
-          <View >
+          <View>
             <Text style={styles.Text1}>Registration</Text>
           </View>
-          <View>
-
-          </View>
-          <Snackbar
-            visible={this.state.snackbarOpen}>
+          <View></View>
+          <Snackbar visible={this.state.snackbarOpen}>
             {this.state.snackbarMessage}
           </Snackbar>
           <TextInput
             value={this.state.firstName}
             onChangeText={this.handleFirstName}
             placeholder={'Firstname'}
-            style={styles.input1}>
-          </TextInput>
+            style={styles.input1}></TextInput>
           <TextInput
             value={this.state.lastName}
             onChangeText={this.handleLastName}
             placeholder={'Lastname'}
-            style={styles.input1}>
-          </TextInput>
+            style={styles.input1}></TextInput>
           <TextInput
             value={this.state.phoneNumber}
             onChangeText={this.handlePhoneNumber}
             placeholder={'Phonenumber'}
-            style={styles.input1}>
-          </TextInput>
+            style={styles.input1}></TextInput>
           <TextInput
             value={this.state.email}
             onChangeText={this.handleEmail}
             placeholder={'Email'}
-            style={styles.input1}>
-          </TextInput>
+            style={styles.input1}></TextInput>
           <TextInput
             value={this.state.password}
             onChangeText={this.handlePassword}
             secureTextEntry={true}
             placeholder={'Password'}
-            style={styles.input1}>
-          </TextInput>
+            style={styles.input1}></TextInput>
           <View style={styles.registerbtn}>
             <Button
               onPress={this.handleRegister}
               title="Register"
-              color="#00B0FF" />
+              color="#00B0FF"
+            />
           </View>
           <View style={styles.loginbtn}>
             <Button
               onPress={() => this.props.navigation.navigate('login')}
               title="Login"
-              color="#00B0FF" />
+              color="#00B0FF"
+            />
           </View>
         </Card>
       </View>
-    )
+    );
   }
 }
-export default RegisterComponent
+export default RegisterComponent;
