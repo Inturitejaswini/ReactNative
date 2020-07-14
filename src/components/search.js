b41bbf4c13790f046b38bde318949132684b6234
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-14 09:36:52 +0530

    [tejaswini]refactor search component functionality

--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Button, Text, TouchableOpacity, TextInput} from 'react-native';
+import {View, Text, TouchableOpacity, TextInput} from 'react-native';
 import styles from '../Styles';
 import {
   ArrowLeftIcon,
