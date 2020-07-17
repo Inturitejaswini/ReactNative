e92bdd975e0c50b733fe78e6542665644214e440
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-17 21:45:38 +0530

    [tejaswini] refactor userSevieces component functionality

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -3,26 +3,49 @@ import configApi from '../constants/userApiConstants';
 import Config from 'react-native-config';
 import AsyncStorage from '@react-native-community/async-storage';
 export default function userRegistration(data) {
-  return axios.post(Config.REACT_APP_BASE_URL + configApi.registration, data);
+  try {
+    return axios.post(Config.REACT_APP_BASE_URL + configApi.registration, data);
+  } catch (err) {
+    return res;
+  }
 }
-
 export function UserLogin(data) {
-  return axios.post(Config.REACT_APP_BASE_URL + configApi.login, data);
+  try {
+    return axios.post(Config.REACT_APP_BASE_URL + configApi.login, data);
+  } catch (err) {
+    return err;
+  }
 }
 export function userForgetPassword(data) {
-  return axios.post(Config.REACT_APP_BASE_URL + configApi.forgetPassword, data);
+  try {
+    return axios.post(
+      Config.REACT_APP_BASE_URL + configApi.forgetPassword,
+      data,
+    );
+  } catch (err) {
+    throw err;
+  }
 }
 
 export function userLogOut(data) {
-  return axios.post(Config.REACT_APP_BASE_URL + configApi.signOut, data);
+  try {
+    return axios.post(Config.REACT_APP_BASE_URL + configApi.signOut, data);
+  } catch (err) {
+    throw err;
+  }
 }
 
 export async function uploadProfile() {
-  let AccessToken = await AsyncStorage.getItem('@storage_Key');
-  let res = axios.post(Config.REACT_APP_BASE_URL + configApi.profileUpdate, {
-    headers: {
-      Authorization: AccessToken,
-    },
-  });
-  return res;
+  try {
+    let AccessToken = await AsyncStorage.getItem('@storage_Key');
+    let res = axios.post(Config.REACT_APP_BASE_URL + configApi.profileUpdate, {
+      headers: {
+        Authorization: AccessToken,
+      },
+    });
+    return res;
+  } 
+  catch (err) {
+    return err;
+  }
 }
